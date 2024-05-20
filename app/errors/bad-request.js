2dc84daf8436bb2240767c2e18d54bd73e6846de
novian <kingboard154@example.com>
2024-05-20 16:17:22 +0700

    first commit

---
+++ app/errors/bad-request.js
@@ -0,0 +1,10 @@
+const { StatusCodes } = require("http-status-codes");
+const CustomAPIError = require("./custom-api-error");
+
+class BadRequest extends CustomAPIError {
+  constructor(message) {
+    super(message);
+    //memberikan statuscode badrequest
+    this.StatusCode = StatusCodes.BAD_REQUEST;
+  }
+}
